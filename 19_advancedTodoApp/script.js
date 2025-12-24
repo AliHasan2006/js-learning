e08c2aca3e46ebca2e5df6cf39c129ae1b3f2083
Ali Hasan Kazmi <alihasankazmi72@gmail.com>
2025-12-24 22:22:57 +0530

    Some Changes

--- 19_advancedTodoApp/script.js
+++ 19_advancedTodoApp/script.js
@@ -1,12 +1,13 @@
 const todo = document.querySelector("#todo");
 const progress = document.querySelector("#progress");
 const done = document.querySelector("#done");
-
+const toggleModalBtn = document.querySelector("#toggle-model");
+const modal = document.querySelector(".modal");
 let dragItem = null;
-
+const rmvModal = document.querySelector(".modal .bg");
 const tasks = document.querySelectorAll('.task');
 const deleteBtn = document.querySelector('#dltBtn');
-
+const createTaskBtn = document.querySelector("#add-task-btn");
 tasks.forEach((task) => {
     task.addEventListener("dragstart", (e) => {
         dragItem = task;
@@ -43,3 +44,38 @@ addDragEventsOnColumn(done);
 //     alert('hi');
 //     console.log(this.parentElement);
 // });
+
+
+// Modal logic
+toggleModalBtn.addEventListener('click',()=>{
+ modal.classList.toggle('active');
+})
+rmvModal.addEventListener('click',()=>{
+    modal.classList.remove('active');
+})
+
+// Task creation Logic
+createTaskBtn.addEventListener('click',()=>{
+    let taskTittle = document.querySelector("#task-input").value;
+    let taskDesc = document.querySelector("#task-desc").value;
+    // console.log(taskDesc,taskTittle);
+    const newTask = document.createElement("div");
+    newTask.classList.add('task')
+    newTask.setAttribute('draggable', 'true');
+    newTask.innerHTML = `
+    <h2>${taskTittle}</h2>
+            <p>${taskDesc}</p>
+            <button id="dltBtn">Delete</button>
+    `
+    newTask.addEventListener("dragstart", (e) => {
+        dragItem = newTask;
+    });
+    todo.appendChild(newTask);
+     modal.classList.remove('active');    
+})
+
+[todo,progress,done].forEach((col)=>{
+    const tasks = col.querySelectorAll("#task");
+    const count = col.querySelector(".right");
+    count.innerText = tasks.length;
+})
